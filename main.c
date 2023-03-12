4e97552d113d26b58dfb9d629d1a5b94b45cb35b
Blattoid <danielbridgewater87@gmail.com>
2023-03-12 11:56:03 +0000

    GPIO read test

--- main.c
+++ main.c
@@ -1,19 +1,42 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define DATA_PIN 3
+#define CLK_PIN 2
+#define OUT1_PIN 0
+#define OUT2_PIN 1
+
 int main() {
-    stdio_init_all();
+  // Initialise USB serial
+  stdio_init_all();
 
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    char blink = 0;
+  // Enable the onboard LED
+  const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+  gpio_put(LED_PIN, 1);
 
-    while (true) {
-        printf("Hello, world!\n");
-        blink ^= 1;
-        gpio_put(LED_PIN, blink);
-        sleep_ms(1000);
-    }
-    return 0;
+  // Configure GPIO
+  char outputs[2] = {OUT1_PIN, OUT2_PIN};
+  for (char pin=0; pin<sizeof(outputs); pin++) {
+    gpio_init(pin);
+    gpio_set_dir(pin, GPIO_OUT);
+    gpio_put(pin, 0);
+  }
+  char inputs[2] = {DATA_PIN, CLK_PIN};
+  for (char pin=0; pin<sizeof(inputs); pin++) {
+    gpio_init(pin);
+    gpio_set_dir(pin, GPIO_IN);
+    gpio_pull_up(pin);
+  }
+
+  while (true) {
+     bool d = gpio_get(DATA_PIN);
+     bool c = gpio_get(CLK_PIN);
+     printf("d=%d c=%d\n", d,c);
+     gpio_put(OUT1_PIN, d);
+     gpio_put(OUT2_PIN, c);
+     sleep_ms(500);
+  }
+  return 0;
 }
