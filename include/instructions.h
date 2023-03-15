d491ce1d7ff69072eb5e42bdb253e578b20b0e30
Blattoid <danielbridgewater87@gmail.com>
2023-03-15 01:43:34 +0000

    Fix opcode name buffer size

--- include/instructions.h
+++ include/instructions.h
@@ -2,7 +2,7 @@
 #define INSTRUCTIONS_H
 
 typedef struct {
-  char name[10]; // Opcode name
+  char name[11]; // Opcode name
   char length; // How many bytes of memory the instruction uses,
   char cycles;  // Base number of cycles required for this instruction. It will always take this long to execute.
   int* execute; // Pointer to the function for this opcode. Returns the number of extra cycles the instruction took on top of the base cycle count, which is usually 0.
