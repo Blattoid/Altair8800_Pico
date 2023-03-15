ab87a32d330b5ff06803a10f4866f15b918667be
Blattoid <danielbridgewater87@gmail.com>
2023-03-15 01:43:01 +0000

    Simplify the NOP and UNDEF opcode functions

--- src/instructions_impl.c
+++ src/instructions_impl.c
@@ -3,11 +3,8 @@
 
 // Placeholder, should do the same as a NOP
 // Make sure to remove it after all the implementations are written!
-int opcode_UNDEF() { 
-  /*return*/ opcode_NOP();
-}
+int opcode_UNDEF() {}
 
 int opcode_NOP() {
-  // No Operation - Waste some cycles
-  //return 0;
+  // No Operation - Do nothing and waste some cycles
 }
