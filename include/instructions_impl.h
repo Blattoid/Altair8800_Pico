36d927eb5f751411a57936b19696bf6ff7426ebc
Blattoid <danielbridgewater87@gmail.com>
2023-04-07 19:38:50 +0100

    Remove dummy opcode

--- include/instructions_impl.h
+++ include/instructions_impl.h
@@ -2,7 +2,6 @@
 #define INSTRUCTIONS_IMPL_H
 
 #define OPCODE_EXTRA_CYCLES 6 // Some instructions take extra cycles. Specifically, 6 cycles. Figured I'd best make it a constant.
-int opcode_UNDEF(); // Delete this after
 
 // List of opcode implementations
 int opcode_ACI_d8();
