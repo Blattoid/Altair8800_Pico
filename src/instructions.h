53e80bb6034b73e9a7cb4394c31453ba5e9f9275
Blattoid <danielbridgewater87@gmail.com>
2023-08-03 01:59:47 +0100

    Fix comment

--- src/instructions.h
+++ src/instructions.h
@@ -5,7 +5,7 @@ typedef struct {
   char name[11]; // Opcode mnemonic
   char length; // How many bytes of memory the instruction uses
   char cycles;  // Base number of cycles required for this instruction. It will always take at least this long to execute.
-  int (*execute)(); // Pointer to the function for this opcode. Returns the number of extra cycles the instruction took on top of the base cycle count, which is usually 0.
+  int (*execute)(); // Pointer to the function for this opcode. Returns the number of extra cycles the instruction took to execute, which is usually 0.
 } Opcode;
 
 extern Opcode instruction_set[];
